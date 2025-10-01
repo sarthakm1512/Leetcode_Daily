74c253fe12e7afe7c8ebc4821e88e9cc96b9fe4b
Sarthak Malviya <120418085+sarthakm1512@users.noreply.github.com>
2025-10-01 22:56:06 +0530

    Find Triangular Sum of An Array solution code added

---
+++ Sept_25/30-09_2221_FindTriangularSumOfAnArray.py
@@ -0,0 +1,46 @@
+#Question Link: https://leetcode.com/problems/find-triangular-sum-of-an-array/description/?envType=daily-question&envId=2025-09-30
+"""
+2221. Find Triangular Sum of an Array
+
+Problem Statement: You are given a 0-indexed integer array nums, where nums[i] is a digit between 0 and 9 (inclusive).
+The triangular sum of nums is the value of the only element present in nums after the following process terminates:
+1. Let nums comprise of n elements. If n == 1, end the process. Otherwise, create a new 0-indexed integer array newNums of length n - 1.
+2. For each index i, where 0 <= i < n - 1, assign the value of newNums[i] as (nums[i] + nums[i+1]) % 10, where % denotes modulo operator.
+3. Replace the array nums with newNums.
+4. Repeat the entire process starting from step 1.
+Return the triangular sum of nums. 
+
+Example 1:
+1 2 3 4 5
+ 3 5 7 9
+  8 2 6
+   0 8
+    8
+Input: nums = [1,2,3,4,5]
+Output: 8
+Explanation:
+The above diagram depicts the process from which we obtain the triangular sum of the array.
+
+Example 2:
+Input: nums = [5]
+Output: 5
+Explanation:
+Since there is only one element in nums, the triangular sum is the value of that element itself.
+ 
+Constraints:
+* 1 <= nums.length <= 1000
+* 0 <= nums[i] <= 9
+"""
+
+from typing import List
+
+class Solution:
+  def triangularSum(self, nums: List[int]) -> int:
+    #keep reducing the array until only 1 element remains
+    while len(nums) > 1:
+      newNums = [] #will store the next row
+      for i in range(len(nums) - 1):
+        #take sum of adjacent elements, mod 10
+        newNums.append((nums[i] + nums[i+1] % 10)
+      nums = newNums #replace old array with new one
+    return nums[0] #only one element remains
